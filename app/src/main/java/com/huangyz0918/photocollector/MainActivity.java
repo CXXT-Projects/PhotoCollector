1e4dda7866f48509e50e229d8aa9ec8ee7d4bede
huangyz0918 <huangyz0918@gmail.com>
2018-10-28 19:17:50 +0800

    ✨ feat: finish the camera configuration, set up the ci.

--- app/src/main/java/com/huangyz0918/photocollector/MainActivity.java
+++ app/src/main/java/com/huangyz0918/photocollector/MainActivity.java
@@ -1,45 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 CXXT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+
 package com.huangyz0918.photocollector;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
+import com.camerakit.CameraKitView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/*
-*
-* */
+import static com.huangyz0918.photocollector.PermissionManager.requestCameraPermissions;
+
+/**
+ * Photo Collector
+ * The application aims to collect the phone model and photos taken by the devices.
+ * Can upload the message to the remote server.
+ * User can set the server address vis settings in this application.
+ *
+ * @author huangyz0918
+ * @since 10/22/2018
+ * <p>
+ * The {@link MainActivity} is the main interface of this application.
+ */
 public class MainActivity extends AppCompatActivity {
-
-    static final int MY_PERMISSIONS_REQUEST_CAMERA = 0x110;
-    static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0x111;
-    static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0x101;
-    static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0x001;
-
     private String photoPath = Environment.getExternalStorageDirectory()
             .getAbsolutePath() + "/temp" + ".png";
 
-    @BindView(R.id.image_view)
-    ImageView imageView;
+    @BindView(R.id.camera)
+    CameraKitView cameraView;
 
     @BindView(R.id.btn_snap)
     Button btnSnap;
@@ -52,37 +74,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        requestCameraPermissions();
+        requestCameraPermissions(MainActivity.this);
         initEvents();
     }
 
     private void initEvents() {
+
         btnSnap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                requestCameraPermissions();
+                requestCameraPermissions(MainActivity.this);
                 if (hasSDCard()) {
-                    startTakePhoto();
+                    cameraView.captureImage(new CameraKitView.ImageCallback() {
+                        @Override
+                        public void onImage(CameraKitView cameraKitView, byte[] bytes) {
+                            Bitmap result = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                            try {
+                                FileOutputStream out = new FileOutputStream(photoPath);
+                                result.compress(Bitmap.CompressFormat.PNG, 100, out);
+                                Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
                 } else {
-                    Toast.makeText(MainActivity.this, "初始化文件失败，无法调用相机拍照！", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "No SD card in your device, " +
+                            "we cannot save the picture to local!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
-        if (resultCode == RESULT_OK && requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(photoPath, options);
-            double radio = Math.max(options.outWidth * 1.0d / 1024f, options.outHeight * 1.0d / 1024f);
-            options.inSampleSize = (int) Math.ceil(radio);
-            options.inJustDecodeBounds = false;
-            imageView.setImageBitmap(BitmapFactory.decodeFile(photoPath, options));
-        }
-
-        super.onActivityResult(requestCode, resultCode, intent);
     }
 
     private boolean hasSDCard() {
@@ -90,13 +111,6 @@ public class MainActivity extends AppCompatActivity {
         return Environment.MEDIA_MOUNTED.equals(state);
     }
 
-    private void startTakePhoto() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        Uri photoURI = FileProvider.getUriForFile(this,
-                getApplicationContext().getPackageName() + ".provider", new File(photoPath));
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -108,39 +122,33 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.btn_settings) {
-            Toast.makeText(this, "233", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Settings opened!", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void requestCameraPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.CAMERA},
-                    MY_PERMISSIONS_REQUEST_CAMERA);
-            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.CAMERA);
-        }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraView.onStart();
+    }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.onResume();
+    }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
+    @Override
+    protected void onPause() {
+        cameraView.onPause();
+        super.onPause();
+    }
 
+    @Override
+    protected void onStop() {
+        cameraView.onStop();
+        super.onStop();
     }
 
 }
---
+++ app/src/main/java/com/huangyz0918/photocollector/PermissionManager.java
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 CXXT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+
+package com.huangyz0918.photocollector;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Permission manager
+ * Manager the required permissions
+ * 1. for the photo shooting and saving.
+ * 2. for the device information collecting.
+ *
+ * @author huangyz0918
+ * @since 10/28/2018
+ */
+public class PermissionManager {
+
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0x110;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0x111;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0x101;
+
+    /**
+     * Permissions for the camera
+     * 1. the camera permissions
+     * 2. the storage permissions, including the write and read permission.
+     */
+    public static void requestCameraPermissions(Activity activity) {
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+            ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    Manifest.permission.CAMERA);
+        }
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+    }
+}
